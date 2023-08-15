3c7628a414a6be652a109dbcd34e1d93f84b08ab
Tobi Samuel <ufomatobi@gmail.com>
2023-08-15 22:11:56 +0100

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,16 @@ server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer({ server: server });
 
@@ -31,8 +41,37 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to cyber chat!");
   }
 
+  db.run(
+    `INSERT into visitors (count, time) VALUES (${numClients}, datetime('now'))`
+  );
+
   ws.on("close", function close() {
     wss.broadcast(`Clients connected ${numClients}`);
     console.log("The client has disconnected");
   });
 });
+
+// Databases
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )  
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down");
+  db.close();
+}
